55532415166d19163abeb16d5eca80afd2fb3033
Jack Perry <125957065+TheExParrot@users.noreply.github.com>
2023-09-09 16:46:06 +1000

    Added Register & Logout Buttons

--- stylometryproject/stylometryapp/views.py
+++ stylometryproject/stylometryapp/views.py
@@ -220,7 +220,7 @@ def register(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username = username, password = password)
             login(request, user)
-            return redirect('home/')
+            return redirect('/home/')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
