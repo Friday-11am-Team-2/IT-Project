f97f7c41955a5ee36f153caf429c594a7ac5359a
Jack Perry <125957065+TheExParrot@users.noreply.github.com>
2023-09-09 16:23:30 +1000

    Added Basic Login/Logout

--- stylometryproject/stylometryapp/views.py
+++ stylometryproject/stylometryapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.urls import reverse
 from django.http import JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 import json
 import random
@@ -10,7 +14,6 @@ from .forms import DocumentForm
 from .models import *
 
 # TO DO - remove CSRF decorators
-
 def home_page_view(request):
     """ Renders the home page """
     return render(request, 'index.html')
@@ -21,18 +24,23 @@ def about_page_view(request):
     return render(request, 'about.html')
 
 
+@login_required
 def profile_page_view(request):
     """ Renders the profile page """
-    profiles = Profile.objects.all()
+    current_user = request.user
+    profiles = Profile.objects.filter(user=current_user)
     return render(request, 'profile.html', {'profiles': profiles})
 
 
+@login_required
 def verify_page_view(request):
     """ Renders the verify page """
-    profiles = Profile.objects.all()
+    current_user = request.user
+    profiles = Profile.objects.filter(user=current_user)
     return render(request, 'verify.html', {'profiles': profiles})
 
 
+@login_required
 @csrf_exempt
 def create_profile(request):
     """ Creates a new profile """
@@ -41,7 +49,7 @@ def create_profile(request):
         new_profile_name = request.POST.get('name')
 
         # Create a new profile
-        profile = Profile(name=new_profile_name)
+        profile = Profile(name=new_profile_name, user=request.user)
         profile.save()
 
         # Return the newly created profile data as JSON
@@ -57,21 +65,23 @@ def create_profile(request):
         return JsonResponse({'error': 'Invalid method'})
 
 
+@login_required
 def get_profile_name(request, profile_id):
     """ Returns the name of the profile with the given ID """
 
     try:
-        profile = Profile.objects.get(pk=profile_id)
+        profile = Profile.objects.get(pk=profile_id, user=request.user)
         return JsonResponse({'name': profile.name})
     except Profile.DoesNotExist:
         return JsonResponse({'name': 'None'})
 
 
+@login_required
 def get_documents(request, profile_id):
     """ Returns the documents for the profile with the given ID """
 
     try:
-        profile = Profile.objects.get(pk=profile_id)
+        profile = Profile.objects.get(pk=profile_id, user=request.user)
         documents = Document.objects.filter(profile=profile)
         documents_data = [{'id': document.id, 'title': document.title} for document in documents]
         return JsonResponse(documents_data, safe=False)
@@ -79,6 +89,7 @@ def get_documents(request, profile_id):
         return JsonResponse([], safe=False)
 
 
+@login_required
 @csrf_exempt 
 def add_profile_docs(request):
     """ Adds documents to a profile """
@@ -94,7 +105,7 @@ def add_profile_docs(request):
             texts = data.get("file_contents")
 
             # Check if the profile exists (you can add more error handling here)
-            profile = Profile.objects.get(pk=profile_id)
+            profile = Profile.objects.get(pk=profile_id, user=request.user)
 
             # Create and save ProfileDocument instances for each file
             for i in range(len(names)):
@@ -109,6 +120,7 @@ def add_profile_docs(request):
             return JsonResponse({"error": str(e)}, status=400)
 
 
+@login_required
 @csrf_exempt 
 def delete_profile(request):
     """ Deletes a profile """
@@ -116,7 +128,7 @@ def delete_profile(request):
     if request.method == "POST" and request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest":
         profile_id = request.POST.get("profile_id")
         try:
-            profile = Profile.objects.get(id=profile_id)
+            profile = Profile.objects.get(id=profile_id, user=request.user)
             profile.delete()
             return JsonResponse({"message": "Profile deleted successfully"})
         except Profile.DoesNotExist:
@@ -125,6 +137,7 @@ def delete_profile(request):
         return JsonResponse({"error": "Invalid request"}, status=400)
     
 
+@login_required
 @csrf_exempt 
 def edit_profile(request, profile_id):
     """ Edits a profile's name """
@@ -132,8 +145,9 @@ def edit_profile(request, profile_id):
     if request.method == "POST" and request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest":
         new_name = request.POST.get("profile_name")
         try:
-            profile = Profile.objects.get(id=profile_id)
+            profile = Profile.objects.get(id=profile_id, user=request.user)
             profile.name = new_name
+            profile.user = request.user
             profile.save()
             return JsonResponse({"message": "Profile updated successfully"})
         except Profile.DoesNotExist:
@@ -142,6 +156,7 @@ def edit_profile(request, profile_id):
         return JsonResponse({"error": "Invalid request"}, status=400)
     
 
+@login_required
 @csrf_exempt
 def delete_document(request, document_id):
     """ Deletes a document """
@@ -156,6 +171,7 @@ def delete_document(request, document_id):
         return JsonResponse({'error': 'An error occurred while deleting the document'}, status=500)
 
 
+@login_required
 @csrf_exempt
 def run_verification(request):
     """ Runs the verification algorithm """
@@ -171,7 +187,7 @@ def run_verification(request):
             texts = data.get("file_contents")
 
             # Check if the profile exists (you can add more error handling here)
-            profile = Profile.objects.get(pk=profile_id)
+            profile = Profile.objects.get(pk=profile_id, user=request.user)
 
             # Get the documents for the profile
             documents = Document.objects.filter(profile=profile)
@@ -191,3 +207,20 @@ def run_verification(request):
         except Exception as e:
             # Handle exceptions and return an error response
             return JsonResponse({"error": str(e)}, status=400)
+        
+
+def register(request):
+    """User Registration"""
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('home/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
