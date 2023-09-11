9d6b0b5eda19ba1c503546d1b64301ce916429e4
Josh Costa <127472742+CoshuaJ@users.noreply.github.com>
2023-09-11 14:42:15 +1000

    Added .txt filter

--- stylometryproject/stylometryapp/static/scripts/uploadButton.js
+++ stylometryproject/stylometryapp/static/scripts/uploadButton.js
@@ -19,32 +19,35 @@ fileInput.addEventListener("change", () => {
 
     // For each file selected, create a list item and add it to the list
     for (const file of fileInput.files) {
-        let reader = new FileReader();
-        let listItem = document.createElement("li");
-        let fileName = file.name;
-        let fileSize = (file.size / 1024).toFixed(1);
+        // Check for .txt file type
+        //if (file.name.toLowerCase().endsWith(".txt")) {
+            let reader = new FileReader();
+            let listItem = document.createElement("li");
+            let fileName = file.name;
+            let fileSize = (file.size / 1024).toFixed(1);
 
-        listItem.innerHTML = `<p>${fileName}</p>`;
+            listItem.innerHTML = `<p>${fileName}</p>`;
 
-        if (fileSize >= 1024) {
-            fileSize = (fileSize / 1024).toFixed(1);
-        }
+            if (fileSize >= 1024) {
+                fileSize = (fileSize / 1024).toFixed(1);
+            }
 
-        fileList.appendChild(listItem);
+            fileList.appendChild(listItem);
 
-        // Read the file content and add it to the arrays
-        reader.onload = (event) => {
-            const fileContent = event.target.result;
-            fileNamesArray.push(fileName);
+            // Read the file content and add it to the arrays
+            reader.onload = (event) => {
+                const fileContent = event.target.result;
+                fileNamesArray.push(fileName);
 
-            // TO DO - ADD TREATMENT FOR FILE TYPES (either restrict to .txt/add more)
-            fileContentArray.push(fileContent);
+                // TO DO - ADD TREATMENT FOR FILE TYPES (either restrict to .txt/add more)
+                fileContentArray.push(fileContent);
 
-            // You can perform further processing with the file content here
-            console.log(`File "${fileName}" content: ${fileContent}`);
-        };
+                // You can perform further processing with the file content here
+                console.log(`File "${fileName}" content: ${fileContent}`);
+            };
 
-        // Read the file as text
-        reader.readAsText(file);
+            // Read the file as text
+            reader.readAsText(file);
+        //}
     }
 });
